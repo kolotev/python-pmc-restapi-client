7ea74b0c9de9f6b7af138fb08acd7cd381e7745d
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2020-02-05 15:14:50 -0500

    core.py was removed as non needed file

--- src/pmc/restapi-client/core.py
+++
@@ -1,8 +0,0 @@
-def demo_function(input_value: int) -> int:
-    """
-    Always describe functionality in a docstring.
-
-        >>> 2 + demo_function(2)
-        4
-    """
-    return input_value
